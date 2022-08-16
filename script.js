c6d32ba106de4823bf087eb36538f2fd3fccc428
Alex Bortnik <bortnik.ar@gmail.com>
2022-08-16 16:58:27 +0400

    Smooth scroll behavior for all browsers

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const navButton = document.querySelector('.btn-mobile-nav');
 navButton.addEventListener('click', () => {
   header.classList.toggle('nav-open');
 });
+
+// Smooth scrolling for Safari
+const links = document.querySelectorAll('a:link');
+
+links.forEach(link => {
+
+
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const id = link.getAttribute('href');
+
+    if (id === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+
+    if (id !== '#' && id.startsWith('#')) {
+      const section = document.querySelector(id);
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile nav
+    if (link.classList.contains('main-nav-link')) {
+      header.classList.toggle('nav-open');
+    }
+  });
+})
